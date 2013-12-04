98543a023f1b6953982c2504d53fd035811f3a56
Ryan VanGundy <ryan.vangundy@gmail.com>
2013-12-04 17:48:23 -0500

    Release v0.0.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
                 tagName            : 'v%VERSION%',
                 tagMessage         : 'Version %VERSION%',
                 push               : true,
-                pushTo             : 'git://github.com/rvangundy/generator-basic.git',
+                pushTo             : '<%= pkg.repository.url %>',
                 gitDescribeOptions : '--tags --always --abbrev=1 --dirty=-d'
             }
         },
